53230a1e7754853cb250e554807a0693a32909e2
daftboston <dsantoyop@gmail.com>
2023-04-13 11:43:49 -0500

    save

--- src/pages/Purchased.jsx
+++ src/pages/Purchased.jsx
@@ -3,6 +3,9 @@
  import { useState, useEffect } from "react"
 import getConfig from "../utils/getConfig"
 import { ListGroup } from "react-bootstrap"
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
  const Purchased = () => {
 
@@ -12,7 +15,8 @@ import { ListGroup } from "react-bootstrap"
 
         axios
         .get("https://e-commerce-api-v2.academlo.tech/api/v1/purchases",getConfig())
-        .then(resp=> setPurchases(resp.data))
+        .then(resp=> {setPurchases(resp.data)
+        console.log(resp.data);})
 
     },[])
 
@@ -20,14 +24,27 @@ import { ListGroup } from "react-bootstrap"
     return( 
     <div>
         <h1>Purchased</h1>
+
+        <Row xs={1} md={2} lg={3} className="py-3">
         {
             purchases.map(item=>(
                 <ListGroup  key={item.id}>
-                    {item.product.title}
+                    <Card style={{ width: '18rem' }}>
+                       <Card.Img variant="top" src={item.product?.images?.[0].url} />
+                       <Card.Body>
+                       <Card.Title>{item.product.title}</Card.Title>
+                       </Card.Body>
+                    </Card>
+                    
+                   
                 </ListGroup>
 
             ))
         }
+
+
+        </Row>
+        
 
     </div>
         )
